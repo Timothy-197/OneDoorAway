e00c9616039d50ae0a5ebd09109a4b740510e39d
Gary Chen <chenhhgary@gmail.com>
2021-12-05 23:20:59 +0800

    add simple story telling in Level00

---
+++ Portal2d/Assets/Scripts/Level0Story.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Level0Story : MonoBehaviour
+{
+    public Text storyText;          // set in inspector
+
+    private void Start()
+    {
+        storyText.text = "";
+        StartCoroutine("TellBackgroundStory");
+    }
+
+    IEnumerator TellBackgroundStory()
+    {
+        yield return new WaitForSeconds(0.5f);
+        storyText.text = "hello";
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+}
