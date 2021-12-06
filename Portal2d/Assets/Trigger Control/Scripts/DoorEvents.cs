b0da1ab27de17f049652d58c218a7d37369b3e26
Timothy197 <1195796206@qq.com>
2021-12-06 17:53:02 +0800

    implement triggers

---
+++ Portal2d/Assets/Trigger Control/Scripts/DoorEvents.cs
@@ -0,0 +1,35 @@
+﻿// this script is attached to the door event gameobject
+// the door event game object should be added to a certain scene if the scene contains the triggers
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEvents : MonoBehaviour
+{
+    public static DoorEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action<int> onPressTrigger; // player press down the trigger
+    public event Action<int> onLeaveTrigger; // player leave the trigger
+
+    public void PressTrigger(int index)
+    {
+        if (onPressTrigger != null)
+        {
+            onPressTrigger(index);
+        }
+    }
+
+    public void LeaveTrigger(int index)
+    {
+        if (onLeaveTrigger != null)
+        {
+            onLeaveTrigger(index);
+        }
+    }
+}
---
+++ Portal2d/Assets/Trigger Control/Scripts/DoorTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [Tooltip("the index should maps to a unique door")]
+    public int triggerIndex;
+    public LayerMask gravityInstances;
+
+    private GameObject activeSp;
+    private GameObject inactiveSp;
+    void Start()
+    {
+        activeSp = this.transform.GetChild(2).gameObject;
+        inactiveSp = this.transform.GetChild(1).gameObject;
+        activeSp.SetActive(false);
+        inactiveSp.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("enter trigger, layer is: " + other.gameObject.layer);
+        if (IsInLayerMask(other.gameObject.layer, gravityInstances)) {
+            // trigger the event to open the door
+            DoorEvents.current.PressTrigger(triggerIndex);
+            // invoke active button sprite
+            activeSp.SetActive(true);
+            inactiveSp.SetActive(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("leave the trigger!");
+        if (IsInLayerMask(other.gameObject.layer, gravityInstances)) {
+            // trigger the event to close the the door
+            DoorEvents.current.LeaveTrigger(triggerIndex);
+            // invoke inactive button sprite
+            activeSp.SetActive(false);
+            inactiveSp.SetActive(true);
+        }
+    }
+    private bool IsInLayerMask(int layerNum, LayerMask layerMask)
+    {
+        return ((layerMask.value & (1 << layerNum)) != 0);
+    }
+}
