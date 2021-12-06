6a8eb7884f05016a4bfe643fe8090f3e9ed6c73d
Timothy197 <1195796206@qq.com>
2021-12-06 18:55:28 +0800

    implement triggers and doors
    I also modify the parameters of the player prefab in the inspector a bit

---
+++ Portal2d/Assets/Trigger Control/Scripts/LevelHoldPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHoldPlayer : MonoBehaviour
+{
+
+    public LayerMask carryObjectLayers;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (IsInLayerMask(collision.gameObject.layer, carryObjectLayers)) {
+            Debug.Log("carry the object with layer: " + collision.gameObject.layer);
+            collision.gameObject.transform.SetParent(this.transform);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (IsInLayerMask(collision.gameObject.layer, carryObjectLayers))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+    private bool IsInLayerMask(int layerNum, LayerMask layerMask)
+    {
+        return ((layerMask.value & (1 << layerNum)) != 0);
+    }
+}
