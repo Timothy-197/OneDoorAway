7ca7d295929dda8819639a2d92b1dc808a197c8a
Gary Chen <chenhhgary@gmail.com>
2021-12-10 22:14:02 +0800

    accomplishment system in progress

---
+++ Portal2d/Assets/Accomplishments/AccomplishmentPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccomplishmentPanel : MonoBehaviour
+{
+    
+
+
+    public void ActivateAccomplishmentPanel()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void DeactivateAccomplishmentPanel()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Portal2d/Assets/Scripts/MenuMouseHoverManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMouseHoverManager : MonoBehaviour
+{
+    public GameObject CheatBtnInfo;
+
+
+    public void CheatBtn_ShowInfo()
+    {
+        CheatBtnInfo.SetActive(true);
+    }
+    public void CheatBtn_HideInfo()
+    {
+        CheatBtnInfo.SetActive(false);
+    }
+}
