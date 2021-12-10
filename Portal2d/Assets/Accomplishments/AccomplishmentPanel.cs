9e43fc3c24445b0b59e0e12c2284e87fc933e4e4
Gary Chen <chenhhgary@gmail.com>
2021-12-10 23:01:55 +0800

    accomplishment show story & lock/unclock feature complete

--- Portal2d/Assets/Accomplishments/AccomplishmentPanel.cs
+++ Portal2d/Assets/Accomplishments/AccomplishmentPanel.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class AccomplishmentPanel : MonoBehaviour
 {
     public Transform AccomplishmentSet;
+    public GameObject AccomplishmentInfoPanel;
+    public Text AccomplishmentStory;
 
     // PlayerPrefs.GetInt(AccomplishmentName, 0)
     //    - 0: Do not collect this accomplishment
@@ -21,20 +23,28 @@ public class AccomplishmentPanel : MonoBehaviour
     };
 
     private Color LOCKED_COLOR = new Color(0.2f, 0.2f, 0.2f);
+    private Color UNLOCKED_COLOR = new Color(1f, 1f, 1f);
 
+    #region AccomplishmentPanel
     public void ActivateAccomplishmentPanel()
     {
         // go through all accomplishments (children of AccomplishmentSet)
         for (int i = 0; i < AccomplishmentSet.childCount; i++)
         {
-            string AccomplishmentName = AccomplishmentSet.GetChild(i).name;
+            Transform accomplishment = AccomplishmentSet.GetChild(i);
+            string AccomplishmentName = accomplishment.name;
 
-            // if we have already collected this accomplishment, do nothing
             if (PlayerPrefs.GetInt(AccomplishmentName, 0) != 1)
             {
-                Transform accomplishment = AccomplishmentSet.GetChild(i);
                 accomplishment.Find("Lock Image").gameObject.SetActive(true);  // activate the lock
                 accomplishment.GetComponent<Image>().color = LOCKED_COLOR;     // make this accomplishment grey
+                accomplishment.GetComponent<Button>().interactable = false;    // do not allow click
+            }
+            else
+            {
+                accomplishment.Find("Lock Image").gameObject.SetActive(false);
+                accomplishment.GetComponent<Image>().color = UNLOCKED_COLOR;
+                accomplishment.GetComponent<Button>().interactable = true;
             }
         }
 
@@ -45,7 +55,27 @@ public class AccomplishmentPanel : MonoBehaviour
     {
         this.gameObject.SetActive(false);
     }
+    #endregion
 
+
+    #region AccomplishmentInfoPanel
+    public void Activate_AccomplishmentInfoPanel(string storyText)
+    {
+        //update story text
+        AccomplishmentStory.text = storyText;
+
+        AccomplishmentInfoPanel.SetActive(true);
+    }
+    public void Deactivate_AccomplishmentInfoPanel()
+    {
+        AccomplishmentStory.text = "No story here...";
+
+        AccomplishmentInfoPanel.SetActive(false);
+    }
+    #endregion
+
+
+    #region SAVE
     static public void UnlockAllAccomplishments()
     {
         for (int i = 0; i < AccomplishmentList.Count; i++)
@@ -61,4 +91,6 @@ public class AccomplishmentPanel : MonoBehaviour
             PlayerPrefs.SetInt(AccomplishmentList[i], 0);
         }
     }
+    #endregion
+
 }
